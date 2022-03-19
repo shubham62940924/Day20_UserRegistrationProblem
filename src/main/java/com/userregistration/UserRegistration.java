af9228a8c2763c194b5a4b3c7e1812e87710de3d
first_name <ss329222@gmail.com>
2022-03-19 13:49:57 +0530

    Added:Passwordminimum8Character

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -13,13 +13,14 @@ public class UserRegistration {
 	private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
 	private static final String PHONE = "^[9]{1}[1]{1}[ ]?[6-9]{1}[0-9]{9}$";
+	private static final String PASSWORD = "^[\\dA-Za-z\\w\\D]{8,}$";
 
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(NAME);
 		return pattern.matches(NAME, fname);
 	}
 
-//enter a valid LastName
+	// enter a valid LastName
 	public boolean validateLastName(String lName) {
 		Pattern pattern = Pattern.compile(NAME);
 		return pattern.matches(NAME, lName);
@@ -31,9 +32,15 @@ public class UserRegistration {
 		return pattern.matches(EMAIL_PATTERN, email);
 	}
 
-//ValidPhoneNumber
+	// ValidPhoneNumber
 	public boolean validatePhoneNumber(String phNumber) {
 		Pattern pattern = Pattern.compile(PHONE);
 		return pattern.matches(PHONE, phNumber);
+	}
+
+	// Password Minimum8 Character
+	public boolean validatePassword(String password) {
+		Pattern pattern = Pattern.compile(PASSWORD);
+		return pattern.matches(PASSWORD, password);
 	}
 }

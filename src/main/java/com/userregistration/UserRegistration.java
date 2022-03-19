71fa7670581f63d2367b4870c2474797bc70f95e
first_name <ss329222@gmail.com>
2022-03-19 13:46:28 +0530

    Added:ValidPhone

--- src/main/java/com/userregistration/UserRegistration.java
+++ src/main/java/com/userregistration/UserRegistration.java
@@ -12,6 +12,7 @@ import java.util.regex.Pattern;
 public class UserRegistration {
 	private static final String NAME = "^[A-Z]{1}[a-z]{2,}$";
 	private static final String EMAIL_PATTERN = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
+	private static final String PHONE = "^[9]{1}[1]{1}[ ]?[6-9]{1}[0-9]{9}$";
 
 	public boolean validateFirstName(String fname) {
 		Pattern pattern = Pattern.compile(NAME);
@@ -28,5 +29,11 @@ public class UserRegistration {
 	public boolean validateEmail(String email) {
 		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
 		return pattern.matches(EMAIL_PATTERN, email);
+	}
+
+//ValidPhoneNumber
+	public boolean validatePhoneNumber(String phNumber) {
+		Pattern pattern = Pattern.compile(PHONE);
+		return pattern.matches(PHONE, phNumber);
 	}
 }
